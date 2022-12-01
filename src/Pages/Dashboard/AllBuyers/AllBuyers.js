8eba125f3750739f3f25ebe9ed28b65f4c670cb9
nakibuddin <nakibaust137@gmail.com>
2022-12-02 01:43:09 +0600

    delete a buyer added

--- src/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AllBuyers = () => {
     let i=1;
@@ -12,6 +14,22 @@ const AllBuyers = () => {
             return data;
         }
     });
+    const handleDelete = id => {
+        const agree = window.confirm(`Are you sure you want to delete this Buyer ?`);
+        if(agree){
+            fetch(`http://localhost:5000/users/${id}`, {
+                method: 'delete',
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount === 1){                                        
+                    toast('Buyer deleted successfully.', {position: toast.POSITION.TOP_CENTER});                                         
+                    refetch();                                      
+                }
+            })
+            .catch(error =>  console.error('my_fetch_delete_error: ', error) );
+        }
+    }
 
     return (
         <div className="overflow-x-auto">
@@ -35,7 +53,7 @@ const AllBuyers = () => {
                         <td>{buyer?.name}</td>
                         <td>{buyer?.email}</td>
                         <td>{buyer?.role}</td>
-                        <td className='text-red-600 font-bold'>Delete</td>
+                        <td> <button onClick={() => handleDelete(buyer?._id)} className='text-red-600 font-bold'> Delete </button> </td>
                     </tr>)
                 }
                 </tbody>
