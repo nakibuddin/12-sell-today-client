ef4a2e02ecf1899d342a3a9e460350e49f2e2647
nakibuddin <nakibaust137@gmail.com>
2022-12-02 01:19:12 +0600

    react query added

--- src/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,16 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useEffect, useState } from 'react';
 
 const AllSellers = () => {
-    const [sellers, setSellers] = useState([]);
+    // const [sellers, setSellers] = useState([]);
     let i=1;
 
-    useEffect(() => {
-        fetch('http://localhost:5000/users/seller')
-        .then(res => res.json())
-        .then(data => setSellers(data))
-        .catch(err => console.log('my_fetch_error: ', err));
-    },[])
-    
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/users/seller')
+    //     .then(res => res.json())
+    //     .then(data => setSellers(data))
+    //     .catch(err => console.log('my_fetch_error: ', err));
+    // },[])
+
+    const {data: sellers = [], refetch} = useQuery({
+        queryKey: ['users'],
+        queryFn: async() =>{
+            const res = await fetch('http://localhost:5000/users/seller');
+            const data = await res.json();
+            return data;
+        }
+    });
+
     const handleDelete = id => {
         
     }
