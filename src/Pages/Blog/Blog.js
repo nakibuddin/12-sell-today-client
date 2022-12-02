0e299ea0030a1b4e65bb8d2cc408fafc04595617
nakibuddin <nakibaust137@gmail.com>
2022-12-02 18:20:44 +0600

    Blog page added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -9,19 +9,25 @@ const Blog = () => {
                 <div className="space-y-4">
                     <details className="w-full border rounded-lg">
                         <summary className="cursor-pointer px-4 py-6 focus:outline-none focus-visible:ring-violet-400">What are the different ways to manage a state in a React application?</summary>
-                        <p className="px-4 py-6 pt-0 ml-4 -mt-4  text-gray-400">Lectus iaculis orci metus vitae ligula dictum per. Nisl per nullam taciti at adipiscing est. </p>
+                        <p className="px-4 py-6 pt-0 ml-4 -mt-4  text-gray-400">Local state is most often managed in React using the useState hook. For example, local state would be needed to show or hide a modal component or to track values for a form component, such as form submission, when the form is disabled and the values of a form's inputs. </p>
                     </details>
                     <details className="w-full border rounded-lg">
                         <summary className="cursor-pointer px-4 py-6 focus:outline-none focus-visible:ring-violet-400">How does prototypical inheritance work?</summary>
-                        <p className="px-4 py-6 pt-0 ml-4 -mt-4  text-gray-400">Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna porttitor egestas tincidunt neque vehicula potenti. </p>
+                        <p className="px-4 py-6 pt-0 ml-4 -mt-4  text-gray-400">The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object. </p>
                     </details>
                     <details className="w-full border rounded-lg">
                         <summary className="cursor-pointer px-4 py-6 focus:outline-none focus-visible:ring-violet-400">What is a unit test? Why should we write unit tests?</summary>
-                        <p className="px-4 py-6 pt-0 ml-4 -mt-4  text-gray-400">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
+                        <p className="px-4 py-6 pt-0 ml-4 -mt-4  text-gray-400">The main objective of unit testing is to isolate written code to test and determine if it works as intended. Unit testing is an important step in the development process, because if done correctly, it can help detect early flaws in code which may be more difficult to find in later testing stages. </p>
                     </details>
                     <details className="w-full border rounded-lg">
                         <summary className="cursor-pointer px-4 py-6 focus:outline-none focus-visible:ring-violet-400">React vs. Angular vs. Vue?</summary>
-                        <p className="px-4 py-6 pt-0 ml-4 -mt-4  text-gray-400">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
+                        <p className="px-4 py-6 pt-0 ml-4 -mt-4  text-gray-400">There are three frameworks for building web applications that every frontend developer has heard about: React, Vue.js, and Angular.
+
+                            React is a UI library, Angular is a fully-fledged front-end framework, while Vue.js is a progressive framework.
+
+                            They can be used almost interchangeably to build front-end applications, but they’re not 100 percent the same, so it makes sense to compare them and understand their differences.
+
+                            Each framework is component-based and allows the rapid creation of UI features. </p>
                     </details>
                 </div>
             </div>
